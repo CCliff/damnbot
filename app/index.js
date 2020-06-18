a28bafdaa9ec92723eb7d36a2a82424ba6f3174b
CCliff <clcliff30@gmail.com>
2020-06-18 14:00:02 -0400

    Changing to post

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
   console.log(JSON.stringify(req));
   res.send('Hello World!');
 });
