7bb9b6cc81df01c23f2dd3cb9e70561f06548bd4
CCliff <clcliff30@gmail.com>
2020-06-18 14:17:52 -0400

    in channel

--- app/index.js
+++ app/index.js
@@ -12,10 +12,32 @@ app.use(bodyParser.json())
 
 app.post('/', (req, res) => {
   const replacement = req.body.text;
-  response = `${replacement}:turbodaaamn::turbodaaamn:${replacement}${replacement}${replacement}:turbodaaamn:${replacement}${replacement}${replacement}:turbodaaamn::turbodaaamn::turbodaaamn:${replacement}${replacement}:turbodaaamn:${replacement}${replacement}:turbodaaamn:${replacement}
-${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn::turbodaaamn:${replacement}:turbodaaamn:${replacement}
-${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn::turbodaaamn::turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn::turbodaaamn:${replacement}
-${replacement}:turbodaaamn::turbodaaamn:${replacement}${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}${replacement}${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}${replacement}:turbodaaamn:${replacement}`;
+  const responseText = `
+    ${replacement}:turbodaaamn::turbodaaamn:${replacement}${replacement}${replacement}:turbodaaamn:${replacement}${replacement}${replacement}:turbodaaamn::turbodaaamn::turbodaaamn:${replacement}${replacement}:turbodaaamn:${replacement}${replacement}:turbodaaamn:${replacement}
+    ${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn::turbodaaamn:${replacement}:turbodaaamn:${replacement}
+    ${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn::turbodaaamn::turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn::turbodaaamn:${replacement}
+    ${replacement}:turbodaaamn::turbodaaamn:${replacement}${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}${replacement}${replacement}:turbodaaamn:${replacement}:turbodaaamn:${replacement}${replacement}:turbodaaamn:${replacement}
+  `;
+  
+  // const response = {
+  //   "token":"5fIuiNjxlyVukYEteEvhsmDR",
+  //   "team_id":"T03EPHHTC",
+  //   "team_domain":"barbariangroup",
+  //   "channel_id":"GR85G94S0",
+  //   "channel_name":"privategroup",
+  //   "user_id":"UAV003VP0",
+  //   "user_name":"mmahmud",
+  //   "command":"/damn",
+  //   "text":":emoji:",
+  //   "response_url":"https://hooks.slack.com/commands/T03EPHHTC/1205586279457/tRmLkdl4LsQyeLToGLYxRw4E",
+  //   "trigger_id":"1193170848019.3499595930.e8daf2d6da28bc96c517873585f11761"
+  // }
+
+  const response = {
+    response_type: "in_channel",
+    text: response,
+  };
+
   res.send(response);
 });
 
