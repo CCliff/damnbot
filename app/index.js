15848c6a5602c674e5eefd63a584dd62deecacbc
CCliff <clcliff30@gmail.com>
2020-06-18 13:55:16 -0400

    heroku test

--- app/index.js
+++ app/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  console.log(JSON.stringify(req));
+  res.send('Hello World!');
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
