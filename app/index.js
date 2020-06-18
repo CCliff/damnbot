49b4888f2207fcad304e487f3209bc5310e42b75
CCliff <clcliff30@gmail.com>
2020-06-18 14:03:42 -0400

    a

--- app/index.js
+++ app/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+
 const app = express();
 const port = process.env.PORT || 3000;
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// app.use(function (req, res) {
+//   res.setHeader('Content-Type', 'text/plain')
+//   res.write('you posted:\n')
+//   res.end(JSON.stringify(req.body, null, 2))
+// })
+
 app.post('/', (req, res) => {
-  console.log(JSON.stringify(req));
+  console.log(JSON.stringify(req.body));
   res.send('Hello World!');
 });
 
